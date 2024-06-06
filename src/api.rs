f88c842f5443b50b4d821c05837c00e6785bd990
Emerson MX <emersonmx@gmail.com>
2024-06-06 18:46:43 -0300

    Move api to module

---
+++ src/api.rs
@@ -0,0 +1,66 @@
+use std::net::SocketAddr;
+
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::{
+    body::{Bytes, Incoming},
+    server::conn::http1,
+    service::service_fn,
+    Method, Request, Response, StatusCode,
+};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+pub async fn run() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let io = TokioIo::new(stream);
+        tokio::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(router))
+                .await
+            {
+                eprintln!("Error serving connect: {:?}", err);
+            }
+        });
+    }
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full(chunk: impl Into<Bytes>) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+async fn router(
+    request: Request<Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (request.method(), request.uri().path()) {
+        (&Method::GET, "/") => hello_handler(request).await,
+        _ => fallback_handler(request).await,
+    }
+}
+
+async fn hello_handler(
+    _: Request<Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    Ok(Response::new(full("Hello World")))
+}
+
+async fn fallback_handler(
+    _: Request<Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let mut not_found = Response::new(empty());
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
+}
