d08009fc50d06190663d65c00252009639aee13d
Emerson MX <emersonmx@gmail.com>
2024-06-07 17:06:45 -0300

    Convert code to use axum

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,52 +1,87 @@
-use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
-use hyper::body::Bytes;
-use hyper::client::conn::http1::Builder;
+use axum::{
+    body::Body,
+    extract::Request,
+    http::{Method, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+
+use hyper::body::Incoming;
 use hyper::server::conn::http1;
-use hyper::service::service_fn;
 use hyper::upgrade::Upgraded;
-use hyper::{Method, StatusCode, Uri};
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
 use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
+use tower::Service;
+use tower::ServiceExt;
+
+use hyper_util::rt::TokioIo;
 
 pub async fn run() {
+    let router_svc = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let tower_service = tower::service_fn(move |req: Request<_>| {
+        let router_svc = router_svc.clone();
+        let req = req.map(Body::new);
+        async move {
+            if req.method() == Method::CONNECT {
+                proxy(req).await
+            } else {
+                router_svc.oneshot(req).await.map_err(|err| match err {})
+            }
+        }
+    });
+
+    let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+        tower_service.clone().call(request)
+    });
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
+    println!("Listening Proxy on {}", addr);
+
     let listener = TcpListener::bind(addr).await.unwrap();
-
-    println!("Listening Proxy on http://{}", addr);
-
     loop {
         let (stream, _) = listener.accept().await.unwrap();
         let io = TokioIo::new(stream);
-        tokio::spawn(async move {
+        let hyper_service = hyper_service.clone();
+        tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
                 .preserve_header_case(true)
                 .title_case_headers(true)
-                .serve_connection(io, service_fn(proxy))
+                .serve_connection(io, hyper_service)
                 .with_upgrades()
                 .await
             {
-                eprintln!("Error serving connect: {:?}", err);
+                println!("Failed to serve connection: {:?}", err);
             }
         });
     }
 }
 
-fn host_addr(uri: &Uri) -> Option<String> {
-    uri.authority().map(|auth| auth.to_string())
-}
+async fn proxy(req: Request) -> Result<Response, hyper::Error> {
+    dbg!(&req);
 
-fn empty() -> BoxBody<Bytes, hyper::Error> {
-    Empty::<Bytes>::new()
-        .map_err(|never| match never {})
-        .boxed()
-}
+    if let Some(host_addr) = req.uri().authority().map(|auth| auth.to_string()) {
+        tokio::task::spawn(async move {
+            match hyper::upgrade::on(req).await {
+                Ok(upgraded) => {
+                    if let Err(e) = tunnel(upgraded, host_addr).await {
+                        println!("server io error: {}", e);
+                    };
+                }
+                Err(e) => println!("upgrade error: {}", e),
+            }
+        });
 
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-    Full::new(chunk.into())
-        .map_err(|never| match never {})
-        .boxed()
+        Ok(Response::new(Body::empty()))
+    } else {
+        println!("CONNECT host is not socket addr: {:?}", req.uri());
+        Ok((
+            StatusCode::BAD_REQUEST,
+            "CONNECT must be to a socket address",
+        )
+            .into_response())
+    }
 }
 
 async fn tunnel(upgraded: Upgraded, addr: String) -> std::io::Result<()> {
@@ -62,51 +97,4 @@ async fn tunnel(upgraded: Upgraded, addr: String) -> std::io::Result<()> {
     );
 
     Ok(())
-}
-
-async fn proxy(
-    req: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    if Method::CONNECT == req.method() {
-        if let Some(addr) = host_addr(req.uri()) {
-            tokio::task::spawn(async move {
-                match hyper::upgrade::on(req).await {
-                    Ok(upgraded) => {
-                        if let Err(e) = tunnel(upgraded, addr).await {
-                            eprintln!("server io error: {}", e);
-                        };
-                    }
-                    Err(e) => eprintln!("upgrade error: {}", e),
-                }
-            });
-
-            Ok(Response::new(empty()))
-        } else {
-            eprintln!("CONNECT host is not socket addr: {:?}", req.uri());
-            let mut resp = Response::new(full("CONNECT must be to a socket address"));
-            *resp.status_mut() = StatusCode::BAD_REQUEST;
-
-            Ok(resp)
-        }
-    } else {
-        let host = req.uri().host().expect("uri has no host");
-        let port = req.uri().port_u16().unwrap_or(80);
-
-        let stream = TcpStream::connect((host, port)).await.unwrap();
-        let io = TokioIo::new(stream);
-
-        let (mut sender, conn) = Builder::new()
-            .preserve_header_case(true)
-            .title_case_headers(true)
-            .handshake(io)
-            .await?;
-        tokio::task::spawn(async move {
-            if let Err(err) = conn.await {
-                println!("Connection failed: {:?}", err);
-            }
-        });
-
-        let resp = sender.send_request(req).await?;
-        Ok(resp.map(|b| b.boxed()))
-    }
 }
