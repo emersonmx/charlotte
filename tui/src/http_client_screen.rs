1cd12fed5e9ab94275040f0473571e56aef78d0d
Emerson MX <emersonmx@gmail.com>
2026-05-06 11:15:16 -0300

    fix: http_client_screen navigation

--- tui/src/http_client_screen.rs
+++ tui/src/http_client_screen.rs
@@ -5,6 +5,7 @@ use ratatui::{
     Frame,
     layout::{Constraint, Direction, Layout, Margin, Rect},
     style::{Style, palette::tailwind},
+    symbols,
     text::{Line, Span},
     widgets::{Block, Cell, Paragraph, Row, Table},
 };
@@ -14,8 +15,7 @@ use std::fmt::Display;
 pub enum Message {
     NextTab,
     PreviousTab,
-    NextSection,
-    PreviousSection,
+    SelectSection(usize),
 }
 
 impl From<Message> for AppMessage {
@@ -30,20 +30,40 @@ enum Tab {
     Response,
 }
 
-impl Tab {
-    fn toggle(&mut self) {
-        *self = match self {
-            Tab::Request => Tab::Response,
-            Tab::Response => Tab::Request,
-        };
-    }
-}
-
 impl Display for Tab {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Tab::Request => write!(f, "Request"),
             Tab::Response => write!(f, "Response"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+enum Section {
+    #[default]
+    Method,
+    Url,
+    QueryParams,
+    Headers,
+    Body,
+}
+
+impl From<RequestSection> for Section {
+    fn from(value: RequestSection) -> Self {
+        match value {
+            RequestSection::QueryParams => Section::QueryParams,
+            RequestSection::Headers => Section::Headers,
+            RequestSection::Body => Section::Body,
+        }
+    }
+}
+
+impl From<ResponseSection> for Section {
+    fn from(value: ResponseSection) -> Self {
+        match value {
+            ResponseSection::Headers => Section::Headers,
+            ResponseSection::Body => Section::Body,
         }
     }
 }
@@ -55,22 +75,14 @@ enum RequestSection {
     Headers,
     Body,
 }
-
-impl RequestSection {
-    fn next(&mut self) {
-        *self = match self {
-            RequestSection::QueryParams => RequestSection::Headers,
-            RequestSection::Headers => RequestSection::Body,
-            RequestSection::Body => RequestSection::QueryParams,
-        };
-    }
-
-    fn previous(&mut self) {
-        *self = match self {
-            RequestSection::QueryParams => RequestSection::Body,
-            RequestSection::Headers => RequestSection::QueryParams,
-            RequestSection::Body => RequestSection::Headers,
-        };
+impl From<usize> for RequestSection {
+    fn from(value: usize) -> Self {
+        match value {
+            3 => RequestSection::QueryParams,
+            4 => RequestSection::Headers,
+            5 => RequestSection::Body,
+            _ => RequestSection::default(),
+        }
     }
 }
 
@@ -81,25 +93,20 @@ enum ResponseSection {
     Body,
 }
 
-impl ResponseSection {
-    fn next(&mut self) {
-        *self = match self {
-            ResponseSection::Headers => ResponseSection::Body,
-            ResponseSection::Body => ResponseSection::Headers,
-        };
-    }
-
-    fn previous(&mut self) {
-        *self = match self {
-            ResponseSection::Headers => ResponseSection::Body,
-            ResponseSection::Body => ResponseSection::Headers,
-        };
+impl From<usize> for ResponseSection {
+    fn from(value: usize) -> Self {
+        match value {
+            3 => ResponseSection::Headers,
+            4 => ResponseSection::Body,
+            _ => ResponseSection::default(),
+        }
     }
 }
 
 pub struct HttpClientScreen {
     request_entry: Option<RequestEntry>,
     tab_selected: Tab,
+    section_selected: Section,
     request_section_selected: RequestSection,
     response_section_selected: ResponseSection,
 }
@@ -115,6 +122,7 @@ impl HttpClientScreen {
         Self {
             request_entry,
             tab_selected: Tab::Request,
+            section_selected: Section::default(),
             request_section_selected: RequestSection::default(),
             response_section_selected: ResponseSection::default(),
         }
@@ -127,20 +135,83 @@ impl HttpClientScreen {
         frame.render_widget(paragraph, frame.area());
     }
 
-    fn draw_method_url(&self, frame: &mut Frame, area: Rect, request_entry: &RequestEntry) {
-        let method_paragraph = Paragraph::new(request_entry.request.method.to_string())
-            .block(Block::bordered().title(Self::METHOD_LABEL));
-        let url_paragraph = Paragraph::new(request_entry.request.url.to_string())
-            .block(Block::bordered().title(Self::URL_LABEL));
+    fn draw_method_url(&self, frame: &mut Frame, area: Rect) {
         let layout = Layout::default()
             .direction(Direction::Horizontal)
             .constraints([Constraint::Length(20), Constraint::Min(0)]);
         let [method_area, url_area] = area.layout(&layout);
-        frame.render_widget(method_paragraph, method_area);
-        frame.render_widget(url_paragraph, url_area);
+
+        self.draw_method(frame, method_area);
+        self.draw_url(frame, url_area);
     }
 
-    fn draw_query_params(&self, frame: &mut Frame, area: Rect, request_entry: &RequestEntry) {
+    fn draw_method(&self, frame: &mut Frame, area: Rect) {
+        let text = match &self.request_entry {
+            Some(entry) => entry.request.method.to_string(),
+            None => "".to_string(),
+        };
+
+        let mut block = Block::bordered().title(format!(
+            "[1]{}{}",
+            symbols::line::HORIZONTAL,
+            Self::METHOD_LABEL
+        ));
+        if self.section_selected == Section::Method {
+            block = block.style(Style::default().fg(tailwind::BLUE.c400));
+        }
+
+        let paragraph = Paragraph::new(text).block(block);
+
+        frame.render_widget(paragraph, area);
+    }
+    fn draw_url(&self, frame: &mut Frame, area: Rect) {
+        let text = match &self.request_entry {
+            Some(entry) => entry.request.url.to_string(),
+            None => "".to_string(),
+        };
+
+        let mut block = Block::bordered().title(format!(
+            "[2]{}{}",
+            symbols::line::HORIZONTAL,
+            Self::URL_LABEL
+        ));
+        if self.section_selected == Section::Url {
+            block = block.style(Style::default().fg(tailwind::BLUE.c400));
+        }
+
+        let paragraph = Paragraph::new(text).block(block);
+
+        frame.render_widget(paragraph, area);
+    }
+
+    fn draw_tabs(&self, frame: &mut Frame, area: Rect) {
+        let tab_titles: Vec<Span> = [Tab::Request, Tab::Response]
+            .iter()
+            .map(|tab| {
+                if self.tab_selected == *tab {
+                    Span::styled(
+                        format!(" {tab} "),
+                        Style::default()
+                            .bg(tailwind::GRAY.c100)
+                            .fg(tailwind::GRAY.c900),
+                    )
+                } else {
+                    Span::styled(format!(" {tab} "), Style::reset())
+                }
+            })
+            .collect();
+
+        let title = Line::from(tab_titles);
+        let tabs = Block::bordered().title(title);
+        frame.render_widget(tabs, area);
+    }
+
+    fn draw_query_params(&self, frame: &mut Frame, area: Rect, select_index: usize) {
+        let request_entry = match &self.request_entry {
+            Some(entry) => entry,
+            None => return,
+        };
+
         let mut width = [0, 0];
         let query_string = request_entry.request.url.query().unwrap_or_default();
         let params: Vec<Row> = query_string
@@ -154,19 +225,19 @@ impl HttpClientScreen {
                 Some(Row::new(vec![Cell::from(key), Cell::from(value)]))
             })
             .collect();
-        let mut title = Line::from(format!(" {} ", Self::QUERY_PARAMS_LABEL));
-        if self.tab_selected == Tab::Request
-            && self.request_section_selected == RequestSection::QueryParams
-        {
-            title = title.style(
-                Style::default()
-                    .bg(tailwind::GRAY.c100)
-                    .fg(tailwind::GRAY.c900),
-            );
+        let title = Line::from(format!(
+            "[{}]{}{}",
+            select_index,
+            symbols::line::HORIZONTAL,
+            Self::QUERY_PARAMS_LABEL
+        ));
+        let mut block = Block::bordered().title(title);
+        if self.section_selected == Section::QueryParams {
+            block = block.style(Style::default().fg(tailwind::BLUE.c400));
         }
         let params_table = Table::default()
             .rows(params)
-            .block(Block::bordered().title(title))
+            .block(block)
             .widths(vec![
                 Constraint::Length(width[0]),
                 Constraint::Min(width[1]),
@@ -175,7 +246,7 @@ impl HttpClientScreen {
         frame.render_widget(params_table, area);
     }
 
-    fn draw_headers(&self, frame: &mut Frame, area: Rect, headers: HeaderMap) {
+    fn draw_headers(&self, frame: &mut Frame, area: Rect, headers: HeaderMap, select_index: usize) {
         let mut widths = [0, 0];
         let headers: Vec<Row> = headers
             .iter()
@@ -187,21 +258,19 @@ impl HttpClientScreen {
                 Row::new(vec![Cell::from(key), Cell::from(value)])
             })
             .collect();
-        let mut title = Line::from(format!(" {} ", Self::HEADERS_LABEL));
-        let section_selected = match self.tab_selected {
-            Tab::Request => self.request_section_selected == RequestSection::Headers,
-            Tab::Response => self.response_section_selected == ResponseSection::Headers,
-        };
-        if section_selected {
-            title = title.style(
-                Style::default()
-                    .bg(tailwind::GRAY.c100)
-                    .fg(tailwind::GRAY.c900),
-            );
+        let title = Line::from(format!(
+            "[{}]{}{}",
+            select_index,
+            symbols::line::HORIZONTAL,
+            Self::HEADERS_LABEL
+        ));
+        let mut block = Block::bordered().title(title);
+        if self.section_selected == Section::Headers {
+            block = block.style(Style::default().fg(tailwind::BLUE.c400));
         }
         let header_table = Table::default()
             .rows(headers)
-            .block(Block::bordered().title(title))
+            .block(block)
             .widths(vec![
                 Constraint::Length(widths[0]),
                 Constraint::Min(widths[1]),
@@ -210,43 +279,70 @@ impl HttpClientScreen {
         frame.render_widget(header_table, area);
     }
 
-    fn draw_body(&self, frame: &mut Frame, area: Rect, body: &[u8]) {
-        let section_selected = match self.tab_selected {
-            Tab::Request => self.request_section_selected == RequestSection::Body,
-            Tab::Response => self.response_section_selected == ResponseSection::Body,
-        };
-        let mut title = Line::from(format!(" {} ", Self::BODY_LABEL));
-        if section_selected {
-            title = title.style(
-                Style::default()
-                    .bg(tailwind::GRAY.c100)
-                    .fg(tailwind::GRAY.c900),
-            );
-        }
-
+    fn draw_body(&self, frame: &mut Frame, area: Rect, body: &[u8], select_index: usize) {
+        let title = Line::from(format!(
+            "[{}]{}{}",
+            select_index,
+            symbols::line::HORIZONTAL,
+            Self::BODY_LABEL
+        ));
         let body_string = String::from_utf8_lossy(body);
-        let paragraph = Paragraph::new(body_string).block(Block::bordered().title(title));
+        let mut block = Block::bordered().title(title);
+        if self.section_selected == Section::Body {
+            block = block.style(Style::default().fg(tailwind::BLUE.c400));
+        }
+        let paragraph = Paragraph::new(body_string).block(block);
         frame.render_widget(paragraph, area);
     }
 
-    fn update_tab_selected(&mut self) -> Option<AppMessage> {
-        self.tab_selected.toggle();
+    fn select_next_tab(&mut self) -> Option<AppMessage> {
+        self.tab_selected = match self.tab_selected {
+            Tab::Request => Tab::Response,
+            Tab::Response => self.tab_selected.clone(),
+        };
+        self.section_selected = match self.section_selected {
+            Section::Method | Section::Url => self.section_selected.clone(),
+            _ => match self.tab_selected {
+                Tab::Request => self.request_section_selected.clone().into(),
+                Tab::Response => self.response_section_selected.clone().into(),
+            },
+        };
         None
     }
 
-    fn update_next_section(&mut self) -> Option<AppMessage> {
-        match self.tab_selected {
-            Tab::Request => self.request_section_selected.next(),
-            Tab::Response => self.response_section_selected.next(),
-        }
+    fn select_previous_tab(&mut self) -> Option<AppMessage> {
+        self.tab_selected = match self.tab_selected {
+            Tab::Request => self.tab_selected.clone(),
+            Tab::Response => Tab::Request,
+        };
+        self.section_selected = match self.section_selected {
+            Section::Method | Section::Url => self.section_selected.clone(),
+            _ => match self.tab_selected {
+                Tab::Request => self.request_section_selected.clone().into(),
+                Tab::Response => self.response_section_selected.clone().into(),
+            },
+        };
         None
     }
 
-    fn update_previous_section(&mut self) -> Option<AppMessage> {
+    fn select_section(&mut self, section_index: usize) -> Option<AppMessage> {
         match self.tab_selected {
-            Tab::Request => self.request_section_selected.previous(),
-            Tab::Response => self.response_section_selected.previous(),
-        }
+            Tab::Request => {
+                self.request_section_selected = section_index.into();
+                self.section_selected = self.request_section_selected.clone().into();
+            }
+            Tab::Response => {
+                self.response_section_selected = section_index.into();
+                self.section_selected = self.response_section_selected.clone().into();
+            }
+        };
+
+        self.section_selected = match section_index {
+            1 => Section::Method,
+            2 => Section::Url,
+            _ => self.section_selected.clone(),
+        };
+
         None
     }
 }
@@ -266,28 +362,9 @@ impl Screen for HttpClientScreen {
             .constraints([Constraint::Length(3), Constraint::Min(3)]);
         let [method_url_area, tabbed_pane_area] = frame.area().layout(&layout);
 
-        self.draw_method_url(frame, method_url_area, request_entry);
+        self.draw_method_url(frame, method_url_area);
 
-        let tab_titles: Vec<Span> = [Tab::Request, Tab::Response]
-            .iter()
-            .map(|tab| {
-                if self.tab_selected == *tab {
-                    Span::styled(
-                        format!(" {tab} "),
-                        Style::default()
-                            .bg(tailwind::GRAY.c100)
-                            .fg(tailwind::GRAY.c900),
-                    )
-                } else {
-                    Span::styled(format!(" {tab} "), Style::reset())
-                }
-            })
-            .collect();
-
-        let title = Line::from(tab_titles);
-        let tabs = Block::bordered().title(title);
-        frame.render_widget(tabs, tabbed_pane_area);
-
+        self.draw_tabs(frame, tabbed_pane_area);
         match self.tab_selected {
             Tab::Request => {
                 let layout = Layout::default()
@@ -304,9 +381,14 @@ impl Screen for HttpClientScreen {
                     })
                     .layout(&layout);
 
-                self.draw_query_params(frame, params_area, request_entry);
-                self.draw_headers(frame, headers_area, request_entry.request.headers.clone());
-                self.draw_body(frame, body_area, request_entry.request.body.as_bytes());
+                self.draw_query_params(frame, params_area, 3);
+                self.draw_headers(
+                    frame,
+                    headers_area,
+                    request_entry.request.headers.clone(),
+                    4,
+                );
+                self.draw_body(frame, body_area, request_entry.request.body.as_bytes(), 5);
             }
             Tab::Response => {
                 let layout = Layout::default()
@@ -323,7 +405,7 @@ impl Screen for HttpClientScreen {
                     Some(response) => response.headers.clone(),
                     None => HeaderMap::new(vec![]),
                 };
-                self.draw_headers(frame, headers_area, headers);
+                self.draw_headers(frame, headers_area, headers, 3);
                 self.draw_body(
                     frame,
                     body_area,
@@ -332,6 +414,7 @@ impl Screen for HttpClientScreen {
                         .as_ref()
                         .map(|response| response.body.as_bytes())
                         .unwrap_or_default(),
+                    4,
                 );
             }
         }
@@ -350,11 +433,10 @@ impl Screen for HttpClientScreen {
                 KeyCode::Right | KeyCode::Char('l') => {
                     return Some(Message::NextTab.into());
                 }
-                KeyCode::Down | KeyCode::Char('j') => {
-                    return Some(Message::NextSection.into());
-                }
-                KeyCode::Up | KeyCode::Char('k') => {
-                    return Some(Message::PreviousSection.into());
+                KeyCode::Char(c) => {
+                    if let Some(d @ 1..=5) = c.to_digit(10) {
+                        return Some(Message::SelectSection(d as usize).into());
+                    }
                 }
                 _ => {}
             }
@@ -370,9 +452,9 @@ impl Screen for HttpClientScreen {
         };
 
         match message {
-            Message::PreviousTab | Message::NextTab => self.update_tab_selected(),
-            Message::NextSection => self.update_next_section(),
-            Message::PreviousSection => self.update_previous_section(),
+            Message::PreviousTab => self.select_previous_tab(),
+            Message::NextTab => self.select_next_tab(),
+            Message::SelectSection(section) => self.select_section(section),
         }
     }
 }
