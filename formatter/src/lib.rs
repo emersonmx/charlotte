ac903ea122e7b23a7963135541c537d29dfb838f
Emerson MX <emersonmx@gmail.com>
2026-05-14 14:32:06 -0300

    test: add more tests to formatter

--- formatter/src/lib.rs
+++ formatter/src/lib.rs
@@ -19,3 +19,73 @@ pub fn format_code(text: &str) -> String {
 pub fn wrap_text(text: &str, max_width: usize) -> String {
     textwrap::fill(text, max_width)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    fn format_non_code_text() {
+        let input = "This is not JSON.";
+        let output = format_code(input);
+        assert_eq!(input, output);
+    }
+
+    #[rstest]
+    fn format_valid_json() {
+        let input = r#"{"name":"Alice","age":30,"city":"New York"}"#;
+        let expected = r#"{
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+}"#;
+
+        let output = format_code(input);
+        assert_eq!(expected, output);
+    }
+
+    #[rstest]
+    fn format_invalid_json() {
+        let input = r#"{"name":"Alice","age":30,"city":"New York}"#;
+        let output = format_code(input);
+        assert_eq!(input, output);
+    }
+
+    #[rstest]
+    fn wrap_short_text() {
+        let input = "Short text.";
+        let output = wrap_text(input, 20);
+        assert_eq!(input, output);
+    }
+
+    #[rstest]
+    fn wrap_long_text() {
+        let input = "This is a long text that should be wrapped to fit within the specified width.";
+        let expected =
+            "This is a long text that\nshould be wrapped to fit\nwithin the specified width.";
+
+        let output = wrap_text(input, 30);
+        assert_eq!(expected, output);
+    }
+
+    #[rstest]
+    fn wrap_text_with_newlines() {
+        let input =
+            "This is a long text\nthat should be wrapped\nto fit within the specified\nwidth.";
+        let expected =
+            "This is a long text\nthat should be wrapped\nto fit within the specified\nwidth.";
+
+        let output = wrap_text(input, 30);
+        assert_eq!(expected, output);
+    }
+
+    #[rstest]
+    fn wrap_text_with_long_word() {
+        let input = "ThisIsAReallyLongWordThatExceedsTheWidthLimit";
+        let expected = "ThisIsARea\nllyLongWor\ndThatExcee\ndsTheWidth\nLimit";
+
+        let output = wrap_text(input, 10);
+        assert_eq!(expected, output);
+    }
+}
